963c19bc47e18ea751a65b24fae707e053bd4800
ADMIN\OOTSUKA <test@ddthink.com>
2017-09-01 19:16:45 +0900

    E3-1のコミット

---
+++ kennsyuu1/src/kennsyuu1/E3_1_if.java
@@ -0,0 +1,30 @@
+package kennsyuu1;
+
+/*
+ * 整数値を読み込んで、その絶対値を求めて表示するプログラムを生成せよ
+ * */
+
+import java.util.Scanner;
+
+public class E3_1_if {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		// 	整数値という文字を表示させる
+		System.out.print("整数値：");
+
+		int n = stdIn.nextInt();
+
+
+
+		public static double abc(double a) {
+			return (a <= 0.0D) ? 0.0D - a ; a;
+		}
+
+
+	if( n < 0)
+		System.out.println("その絶対値は" + n + "です。");
+
+
+	}
+
