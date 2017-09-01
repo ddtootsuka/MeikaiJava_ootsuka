6c3ab8a3acda2ebb793be7389dea56a53f91303a
ADMIN\OOTSUKA <test@ddthink.com>
2017-09-01 21:26:35 +0900

    E2-7再コミット ・修正完了しました

--- kennsyuu1/src/kennsyuu1/E2_7_Ransuu.java
+++ kennsyuu1/src/kennsyuu1/E2_7_Ransuu.java
@@ -2,7 +2,7 @@ package kennsyuu1;
 
 /*
  * 以下に示すプログラムを作成せよ。
- * ・一桁の正の整数値（すなわち1以上9以下の値）をランダムい生成して表示。
+ * ・一桁の正の整数値（すなわち1以上9以下の値）をランダムに生成して表示。
  * ・一桁の負の整数値（すなわち-9以上-1以下の値）をランダムに生成して表示。
  * ・二桁の正の整数値（すなわち10以上99以下の値）をランダムに生成して表示。
  *
@@ -18,11 +18,11 @@ public class E2_7_Ransuu {
 		// 乱数の生成を行う
 		Random rand = new Random();
 		// 1桁の1以上9以下の値をランダムに表示させる
-		int lucky = rand.nextInt(10);
+		int lucky = rand.nextInt(10)+1;
 		// 1桁の-9以下-1以下の値をランダムに表示させる
-		int lucky2 = rand.nextInt(10)-10;
+		int lucky2 = rand.nextInt(8)-8;
 		// 2桁の10以上99以下の値をランダムに表示させる
-		int lucky3 = rand.nextInt(99);
+		int lucky3 = rand.nextInt(89)+10;
 
 
 		// ランダムに生成される数字を表示させる
