15d69a18c6e80aa73e553c3ef871fef940630331
ADMIN\OOTSUKA <test@ddthink.com>
2017-10-11 20:05:58 +0900

    再度修正いたしました。教えて頂いた通り修正しようと思ったのですが、教本通り進めると「private static Scanner]というプログラムはまだ出てきていませんでしたので、教本に合わせ修正いたしました。コミットします。よろしくお願いいたします。

--- kennsyuu1/src/kennsyuu1/E2_3_Hensuu.java
+++ kennsyuu1/src/kennsyuu1/E2_3_Hensuu.java
@@ -8,12 +8,14 @@ package kennsyuu1;
 import java.util.Scanner;
 // キーボードから読み込んだ整数値を反復して表示するプログラムを作成するクラス
 public class E2_3_Hensuu {
-	//整数値をそのまま反復して表示させる
-	private static Scanner NUM1;
+	
+	
 	//キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成します
 	public static void main(String[] args) {
+		Scanner NUM1 = new Scanner(System.in);
+		
 		// 整数値を表示させる出力コードを入力します
-		System.out.println("整数値：");
+		System.out.println("整数値：7");
 		// 整数値を横に表示させる文字を表示させます　
 		System.out.println("と入力しましたね");
 		// キーボードと結びつく入力ストリームを入力します
