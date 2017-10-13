67e2a82ab88865f62df1418e3fe0c1b95bd5a731
ADMIN\OOTSUKA <test@ddthink.com>
2017-10-13 21:14:43 +0900

    修正完了しました。コミットします。 宜しくお願いいたします。

--- kennsyuu1/src/kennsyuu1/E2_3_Hensuu.java
+++ kennsyuu1/src/kennsyuu1/E2_3_Hensuu.java
@@ -1,4 +1,5 @@
-package kennsyuu1;
+package
+kennsyuu1;
 
 /*
  *右に示すように、キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成せよ。
@@ -8,20 +9,24 @@ package kennsyuu1;
 import java.util.Scanner;
 // キーボードから読み込んだ整数値を反復して表示するプログラムを作成するクラス
 public class E2_3_Hensuu {
-	
-	
-	//キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成します
-	public static void main(String[] args) {
-		Scanner NUM1 = new Scanner(System.in);
+
+
+	    //キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成します
+	    public static void main(String[] args) {
+		Scanner numScanner = new Scanner(System.in);
 		
 		// 整数値を表示させる出力コードを入力します
-		System.out.println("整数値：7");
-		// 整数値を横に表示させる文字を表示させます　
-		System.out.println("と入力しましたね");
-		// キーボードと結びつく入力ストリームを入力します
-		NUM1 = new Scanner(System.in);
-		// 文字を代入します
-		NUM1.nextInt();
+		System.out.println("整数値：");
+		
+		 //number1にスキャナーから出力した値を入れます
+		int number1 = numScanner.nextInt();
+		
+		
+		
+		// 整数値と指定した文を横に表示させる文字を表示させます　
+		System.out.println( number1 + "と入力しましたね");
 
 	}
 }
+
+ 
