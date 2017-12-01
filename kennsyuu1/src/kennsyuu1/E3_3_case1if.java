e62913a7e4d36a5601d6ac5b37c6950bde2f83c7
ADMIN\OOTSUKA <test@ddthink.com>
2017-12-01 20:42:07 +0900

    E3_3再度修正致しました。よろしくお願い致します。

--- kennsyuu1/src/kennsyuu1/E3_3_case1if.java
+++ kennsyuu1/src/kennsyuu1/E3_3_case1if.java
@@ -1,35 +1,44 @@
 package kennsyuu1;
 
+import java.util.Scanner;
 
-	import java.util.Scanner;
+/*
+ * List3-5の最後のelseを、else if(n==0)に変更したらどうなるか検討せよ。
+ * */
 
-	/*
-	 * List3-5の最後のelseを、else if(n==0)に変更したらどうなるか検討せよ。
-	 * */
+public class E3_3_case1if {
 
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
 
-		public class E3_3_case1if {
+		// 指定された整数値という文字を表示させます
+		System.out.print("整数値:");
 
-			public static void main(String[] args) {
-				Scanner stdIn = new Scanner(System.in);
+		// number1に文字を読み込ませます
+		int number1 = stdIn.nextInt();
 
-				System.out.print("整数値:");
+		// 0とnumber1がtrueであるか判別させる
+		if (number1 > 0)
 
-				int number1 = stdIn.nextInt();
+		// trueであれば指定された文字を表示させる
+		System.out.println("その値は正です。");
 
-				if( number1 > 0 )
-				System.out.println("その値は正です。");
+		// 0と入力した数値がfaiseであるか判別させる
+		else if (number1 < 0)
 
-				else if ( number1 < 0)
-					System.out.println("その値は負です。");
-				else if(number1 == 0)
-					System.out.println("その値は0です。");
+		// falseであれば指定された文字を表示させる
+        System.out.println("その値は負です。");
 
+		// 0と入力した数値が等しい値か判別させる
+		else if (number1 == 0)
 
+		// 0とイコールの値であれば指定された文字を表示する
+		System.out.println("その値は0です。");
 
+		/*
+		 * 結果としては、そのまま０は表示される。 elseの場合も結果は０になりますし、0が対象となっている限り if ( n == 0
+		 * )があっても同じ結果となる。
+		 */
 
-			}
 	}
-
-
-
+}
