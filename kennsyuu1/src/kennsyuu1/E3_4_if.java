630f2847618a74e429329ba49f4ec3cf937bb5ee
ADMIN\OOTSUKA <test@ddthink.com>
2017-12-01 21:24:18 +0900

    E3_4　コミットします。ご確認の程宜しくお願い致します。

--- kennsyuu1/src/kennsyuu1/E3_4_if.java
+++ kennsyuu1/src/kennsyuu1/E3_4_if.java
@@ -15,21 +15,31 @@ public class E3_4_if {
 		Scanner stdIn = new Scanner(System.in);
 
 		// 変数aに値を入れて、画面に変数aと表示させます
-		System.out.print("変数a:"); int a = stdIn.nextInt();
+		System.out.print("変数a:");
+
+		// 変数Aに文字を読み込みます
+		int variableA = stdIn.nextInt();
 		// 変数bに値を入れて、画面に変数bと表示させます
-		System.out.print("変数b:"); int b = stdIn.nextInt();
+		System.out.print("変数b:");
 
+		// 変数Bに文字を読み込ませます
+		int variableB = stdIn.nextInt();
 
-		if ( a > b )
-		// aの方が大きい場合、"aのほうが大きいです。"と表示させます
-		System.out.println("aのほうが大きいです。");
-		// bの方が大きい場合、"bのほうが大きいです。"と表示させます
-		else if ( a < b )
-		System.out.println("bのほうが大きいです。");
-		// 値が同じ場合は、"aとbは同じ値です"と表示させます
+		// 変数A>変数Bはtrueであるか判別させます
+		if (variableA > variableB)
+
+			// Aの方が大きい場合、"Aのほうが大きいです。"と表示させます
+			System.out.println("Aのほうが大きいです。");
+
+		// 変数A>変数Bはfalseであるか判別させます
+		else if (variableA < variableB)
+			// Bの方が大きい場合、"Bのほうが大きいです。"と表示させます
+			System.out.println("Bのほうが大きいです。");
+
+		// そうでもない場合
 		else
-		System.out.println("aとbは同じ値です");
-
+			// 値が同じ場合は、"AとBは同じ値です"と表示させます
+			System.out.println("AとBは同じ値です");
 
 	}
 }
