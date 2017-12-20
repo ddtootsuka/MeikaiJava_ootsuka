651e301cc51c12e57920b2d81c0c565a21e3a813
ADMIN\OOTSUKA <test@ddthink.com>
2017-12-20 21:14:24 +0900

    E3_5再修正しました。ご確認をお願いいたします。

--- kennsyuu1/src/kennsyuu1/E3_5_if.java
+++ kennsyuu1/src/kennsyuu1/E3_5_if.java
@@ -9,24 +9,35 @@ import java.util.Scanner;
  * */
 
 public class E3_5_if {
-
+	// 正の整数値を読み込んで、その数値が5で割り切れる数値であるかどうかを判別し、表示させるクラス
 	public static void main(String[] args) {
 		// キーボードを入力させます
 		Scanner stdIn = new Scanner(System.in);
 		// 画面に整数値:と表示させます
 		System.out.print("整数値:");
-		// 変数aに値を入力させます
+		// 読み込んだ数値が5で割り切れる数字かどうかか判別させるために変数aに値を入力させます
 		int IntegerA = stdIn.nextInt();
 
-		// trueであるか判別させる
-		if (IntegerA % 5 == 0)
-			// 5で割り切れた際に、"その値は5で割り切れます。"と表示させます
-			System.out.println("その値は5で割り切れます。");
-		else
-			// 5で割り切れなかった際に、"その値は5で割り切れません。"と表示させます
-			System.out.println("その値は5で割り切れません。");
-		if (IntegerA < 0)
-			// 0以下の値が入った際に、"正でない値が入力されました。"と表示させます
+		// 変数Aが正数であるかをまず判別させる
+		if (IntegerA > 0) {
+
+			// 変数Aが5で割り切れる数値か判別させる {
+			if (IntegerA % 5 == 0) {
+
+				// 5で割り切れた際に、"その値は5で割り切れます。"と表示させます
+				System.out.println("その値は5で割り切れます。");
+
+			} else {
+				// 5で割り切れなかった際に、"その値は5で割り切れません。"と表示させます
+				System.out.println("その値は5で割り切れません。");
+
+			}
+		}
+
+		else if (IntegerA < 1) {
+			// 0以下の値が入った際ｎに、"正でない値が入力されました。"と表示させます
 			System.out.println("正でない値が入力されました。");
+		}
+
 	}
 }
