1d446aec575340c54a02e359b56307b3bfb4d26a
ADMIN\OOTSUKA <test@ddthink.com>
2017-12-27 21:13:57 +0900

    E3_8、修正致しました。

--- kennsyuu1/src/kennsyuu1/E3_8_if.java
+++ kennsyuu1/src/kennsyuu1/E3_8_if.java
@@ -12,30 +12,30 @@ public class E3_8_if {
 	public static void main(String[] args) {
 		// キーボードのスキャナーを入力します
 		Scanner stdIn = new Scanner(System.in);
-		//点数;と表示させる
+		//点数：と表示させる
 		System.out.print("点数：");
 
 
-		//点数Aに値を入力します
-		int tensuA = stdIn.nextInt();
+		//判定される点数に値を入力します
+		int PointsToBeJudgement = stdIn.nextInt();
 
 		//0より大きく、59より小さい値かを判別させる
-		if (tensuA >= 0 && tensuA<= 59 ) {
+		if (PointsToBeJudgement >= 0 && PointsToBeJudgement<= 59 ) {
 			//0～59の数値の場合、不可と表示させる
 			System.out.println("不可");
 
 			//60より大きく、69より小さい値かを判別させる
-		}else if (tensuA >= 60 && tensuA <=69) {
+		}else if (PointsToBeJudgement >= 60 && PointsToBeJudgement <=69) {
 			//60～69の数値の場合、可と表示させる
 			System.out.println("可");
 
 			//70より大きく、79より小さい値かを判別させる
-		}else if(tensuA >= 70 && tensuA <=79) {
+		}else if(PointsToBeJudgement >= 70 && PointsToBeJudgement <=79) {
 			//70～79の数値の場合、良と表示させる
 			System.out.println("良");
 
 			//80より大きく、100より小さい値かを判別させる
-		}else if (tensuA >= 80 && tensuA <=100) {
+		}else if (PointsToBeJudgement >= 80 && PointsToBeJudgement <=100) {
 			//80～100の数値の場合、優と表示させる
 			System.out.println("優");
 		}
