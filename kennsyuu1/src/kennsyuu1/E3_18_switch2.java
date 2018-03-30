f8535a246278a76bb70240e2a6a9204b19d90b47
user-THINK\user <ddt.ootsuka@gmail.com>
2018-03-30 19:49:30 +0900

    E3_18 コミット二回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E3_18_switch2.java
+++ kennsyuu1/src/kennsyuu1/E3_18_switch2.java
@@ -18,41 +18,77 @@ public class E3_18_switch2 {
 
 		// 11~3の場合は冬、4~6の場合は春、7~9の場合は夏、10の場合は秋と表示させよう
 		switch (seasons) {
+		// seasonsに1が入力された場合、"冬"と表示される
 		case 1:
 			System.out.println("冬");
+			// 1が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに2が入力された場合、"冬"と表示される
 		case 2:
 			System.out.println("冬");
+			// 2が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに3が入力された場合、"冬"と表示される
 		case 3:
 			System.out.println("冬");
+			// 3が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに4が入力された場合、"春"と表示される
 		case 4:
 			System.out.println("春");
+			// 4が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに5が入力された場合、"春"と表示される
 		case 5:
 			System.out.println("春");
+			// 5が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに6が入力された場合、"春"と表示される
 		case 6:
 			System.out.println("春");
+			// 6が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに7が入力された場合、"夏"と表示される
 		case 7:
 			System.out.println("夏");
+			// 7が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに8が入力された場合、"夏"と表示される
 		case 8:
 			System.out.println("夏");
+			// 8が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに9が入力された場合、"夏"と表示される
 		case 9:
 			System.out.println("夏");
+			// 9が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに10が入力された場合、"秋"と表示される
 		case 10:
 			System.out.println("秋");
+			// 10が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに11が入力された場合、"冬"と表示される
 		case 11:
 			System.out.println("冬");
+			// 11が入力された場合は、switch文が終了となる
 			break;
+
+		// seasonsに12が入力された場合、"冬"と表示される
 		case 12:
 			System.out.println("冬");
+			// 12が入力された場合は、switch文が終了となる
+			break;
 
 		}
 	}
