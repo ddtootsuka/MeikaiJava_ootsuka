c1d35dad145d4ce95128b2312df6c91f2d0df2aa
user-THINK\user <ddt.ootsuka@gmail.com>
2018-03-30 20:29:49 +0900

    E3_17 　コミット二回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E_3_17_switch.java
+++ kennsyuu1/src/kennsyuu1/E_3_17_switch.java
@@ -17,16 +17,24 @@ public class E_3_17_switch {
 		// 画面に下記、文を表示させます
 		System.out.print("手を表示せよ(0・・グー/1・・チョキ/2・・パー)" + Ransuu + "：");
 
-		
+		// ０の時はグー、１の時はチョキ、２の時はパーを出すように表示します
 		switch (Ransuu) {
+		// Ransuuに0が入る場合、"グー"と表示される
 		case 0:
 			System.out.println("グー");
+			// 0が入力された場合、ｓｗｉｔｃｈ文が終了となる
 			break;
+
+		// Ransuuに1が入る場合、"チョキ"と表示される
 		case 1:
 			System.out.println("チョキ");
+			// 1が入力された場合、ｓｗｉｔｃｈ文が終了となる
 			break;
+
+		// Ransuuに2が入る場合、"パー"と表示される
 		case 2:
 			System.out.println("パー");
+			// 2が入力された場合、ｓｗｉｔｃｈ文が終了となる
 			break;
 		}
 	}
