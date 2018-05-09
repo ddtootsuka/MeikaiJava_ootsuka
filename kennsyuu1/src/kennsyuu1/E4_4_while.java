b5cf3963d3d0e2b318a108f6a795834a0ad3ea7c
user-THINK\user <ddt.ootsuka@gmail.com>
2018-05-09 19:48:16 +0900

    E4_4　コミット４回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E4_4_while.java
+++ kennsyuu1/src/kennsyuu1/E4_4_while.java
@@ -10,30 +10,36 @@ import java.util.Scanner;
 public class E4_4_while {
 
 	public static void main(String[] args) {
-		//キーボードに打ち込んだ文字を入力する
+		// キーボードに打ち込んだ文字を入力する
 		Scanner stdIn = new Scanner(System.in);
 
-		//画面に"カウントダウンします"と表示させます
+		// 画面に"カウントダウンします"と表示させます
 		System.out.println("カウントダウンします");
-		
-		//打ち込む数字
-		int countnumber;
-		//do文を実行する
+
+		// 打ち込む数字をxとします
+		int x;
+		// do文を実行する
 		do {
-			//画面に"正の整数値"と表示させる
+			// 画面に"正の整数値"と表示させる
 			System.out.print("正の整数値:");
-			
-			//打ち込む数字に値を代入する
-			countnumber = stdIn.nextInt();
-			//打ち込む数字が0以下であれば繰り返す
-		} while (countnumber <= 0);
-		
-		//打ち込んだ値を0までカウントダウンさせます
-		while (countnumber >= 0) { 
-			//打ち込んだ数を画面に表示させます
-			System.out.println(countnumber);
-			//打ち込んだ値をデクリメントする
-			countnumber--;			
-					}
+
+			// 打ち込む数字に値を代入する
+			x = stdIn.nextInt();
+			// 打ち込む数字が0以下であれば繰り返す
+		} while (x <= 0);
+
+		// 打ち込んだ値を0までカウントダウンさせます
+		while (x >= 0) {
+			// 打ち込んだ数を画面に表示させます
+			System.out.println(x);
+			// 打ち込んだ値をデクリメント(-1)する
+			x--;
+		}
+		// 画面に"xの値は-1である"と表示させる
+		System.out.println("xの値は-1である");
 	}
 }
+
+// （西脇さんへ）do文とwhile文の違いにつきまして・・・
+// doは実行せよ、whileは～のあいだという意味で、両方条件が成立する間処理を繰り返す文だと思います（式を評価した値がｔｒｕｅである限りエンドレスループ）
+// do文はWhileとセットで動きますが、ｗｈｉｌｅ文はｗｈｉｌｅ単品で繰り返し実行する文となっているかと思います。違いは左記だと思うのですが如何でしょうか？？
