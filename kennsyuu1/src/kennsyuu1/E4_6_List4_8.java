25e0b9ae9f05c1f8145270c60c496736a9cc490e
user-THINK\user <ddt.ootsuka@gmail.com>
2018-06-18 20:22:27 +0900

    E4_6 List4_8（分割しました） コミット１回目。

---
+++ kennsyuu1/src/kennsyuu1/E4_6_List4_8.java
@@ -0,0 +1,35 @@
+package kennsyuu1;
+
+import java.util.Scanner;
+
+/*
+ * 読み込んだ値が1未満であれば改行文字を出力しないように
+ * List4-7およびList4-8を書きかえたプログラムをそれぞれ作成せよ。
+ */
+
+public class E4_6_List4_8 {// 処理を行うメインメソッド
+	public static void main(String[] args) {
+		// キーボードに打ち込んだ文字を入力する
+		Scanner stdIn = new Scanner(System.in);
+
+		// 画面に"何個*を表示しますか？："と表示させる
+		System.out.print("何個*を表示しますか？：");
+		// ｎに値を代入する
+		int n = stdIn.nextInt();
+
+		// 変数iに1を代入する
+		int i = 1;
+		// ｎが1より大きければ、または同じ数であれば繰り返す
+		while (i <= n) {
+			// 画面に'*'を表示させる
+			System.out.println('*');
+			// iの値をインクリメントする
+			i++;
+
+			// 改行を表示する
+			System.out.println("改行テスト");
+		}
+		if (1 > 0)
+			System.out.println();
+	}
+}
