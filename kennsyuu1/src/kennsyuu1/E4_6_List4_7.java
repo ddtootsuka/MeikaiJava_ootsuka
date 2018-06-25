89a1f86cfdf1c50cbb9110c154752fadf124496c
user-THINK\user <ddt.ootsuka@gmail.com>
2018-06-25 21:08:47 +0900

    E4_6_List4_7コミット4回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E4_6_List4_7.java
+++ kennsyuu1/src/kennsyuu1/E4_6_List4_7.java
@@ -9,8 +9,8 @@ import java.util.Scanner;
 
 public class E4_6_List4_7 {
 
-	//まずList4-7の修正をいたします
-	
+	// まずList4-7の修正をいたします
+
 	// 処理を行うメインメソッド
 	public static void main(String[] args) {
 		// キーボードに打ち込んだ文字を入力する
@@ -25,15 +25,18 @@ public class E4_6_List4_7 {
 		int i = 0;
 		// ｎが０より大きければ繰り返す
 		while (i < n) {
-			//画面に'*'を表示させる
+			// 画面に'*'を表示させる
 			System.out.println('*');
-			//iの値をインクリメントする
+			// iの値をインクリメントする
 			i++;
-	
-		//（わかりやすくするために改行テストの文字を入力する）改行を表示する
-		System.out.println("改行テスト\n改行テスト");
+
+			// （わかりやすくするために改行テストの文字を入力する）改行を表示する
+			System.out.println("改行テスト\n改行テスト");
 		}
-	if(i > 0);
-	System.out.println();
-}
+		// iが0より小さい場合、
+		if (i > 0) {
+			// 改行文字を出力しない
+			System.out.println("改行");
+		}
+	}
 }
