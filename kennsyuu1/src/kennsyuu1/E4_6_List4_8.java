b796d671a3e92969be50d820835e10a6ce9ff30f
user-THINK\user <ddt.ootsuka@gmail.com>
2018-06-25 20:01:50 +0900

    E4_6 List4_8（分割しました） コミット3回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E4_6_List4_8.java
+++ kennsyuu1/src/kennsyuu1/E4_6_List4_8.java
@@ -14,22 +14,22 @@ public class E4_6_List4_8 {// 処理を行うメインメソッド
 
 		// 画面に"何個*を表示しますか？："と表示させる
 		System.out.print("何個*を表示しますか？：");
-		// ｎに値を代入する
-		int n = stdIn.nextInt();
+		// kosuu(個数)に値を代入する
+		int kosuu = stdIn.nextInt();
 
-		// 変数iに1を代入する
-		int i = 1;
-		// ｎが1より大きければ、または同じ数であれば繰り返す
-		while (i <= n) {
+		// 変数(variable)に1を代入する
+		int variable = 1;
+		// kosuu(個数)が1より大きければ、または同じ数であれば繰り返す
+		while (variable <= kosuu) {
 			// 画面に'*'を表示させる
 			System.out.println('*');
-			// iの値をインクリメントする
-			i++;
+			// 変数(variable)の値をインクリメントする
+			variable++;
 		}
-		// iが0より小さい場合、
-		if (1 > 0) {
-			//改行文字を出力しない
+		// 変数(variable)が0より小さい場合、
+		if (variable > 0) {
+			// 改行文字を出力しない
 			System.out.println();
+		}
 	}
-}
 }
