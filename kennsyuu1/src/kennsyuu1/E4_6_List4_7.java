504e1e5e64825d46e11356b4af971a25384826f9
user-THINK\user <ddt.ootsuka@gmail.com>
2018-07-18 20:24:49 +0900

    E4_6_List4_7コミット7回目。修正しました。

--- kennsyuu1/src/kennsyuu1/E4_6_List4_7.java
+++ kennsyuu1/src/kennsyuu1/E4_6_List4_7.java
@@ -20,8 +20,6 @@ public class E4_6_List4_7 {
 		System.out.print("何個*を表示しますか？：");
 		// アスタリスクを表示させる個数に値を代入する
 		int number = stdIn.nextInt();
-		
-		
 
 		// 個数と比較させる値に1を代入する
 		int variable = 1;
@@ -35,21 +33,13 @@ public class E4_6_List4_7 {
 			// （わかりやすくするために改行テストの文字を入力する）改行を表示する
 			System.out.println("改行テスト\n改行テスト");
 		}
-		// 読み込んだ値が1未満であれば改行文字を出力しないように、もし比較させる値が1、
+		// 読み込んだ値が1未満であれば改行文字を出力しないように、もし比較させる値が1以上であれば改行をおこなう
 		if (variable > 1) {
 			// 改行文字を出力しない
 			System.out.println("改行");
-			
-			
-			if(number > 1) {
-				System.out.println("改行しない")
-				｝
-				if(number <= 1)
-				
+			if (number <= 1) {
+				System.out.println("改行しない");
 			}
-			
-			
-			
 		}
 	}
 }
