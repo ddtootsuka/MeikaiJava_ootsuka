b26ace2ffb3fda206a69021229bbb16ef1c1f5bb
user-THINK\user <ddt.ootsuka@gmail.com>
2018-07-30 20:06:00 +0900

    再E1_1　１回目　コミットします。

---
+++ kennsyuu1/src/kennsyuu1/E1_1_kouzou.java
@@ -0,0 +1,17 @@
+package kennsyuu1;
+
+/*
+ *プログラムの中の終端を示すセミコロン；が欠如しているとどうなるか。プログラムをコンパイルして検証せよ。 
+ */
+
+public class E1_1_kouzou {
+	//メインメソッドの宣言をします
+	public static void main(String[] args) {
+		//画面に"初めてのJavaプログラム。"と表示させる。
+		System.out.println("初めてのJavaプログラム。");
+		//画面に"画面に出力しています。"と表示させる。
+		System.out.println("画面に出力しています。");
+	}	
+}
+
+//結果は、セミコロンが欠如するとエラーとなる、でした。
