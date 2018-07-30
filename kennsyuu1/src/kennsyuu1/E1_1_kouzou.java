f36067c7a6c85e72f3697d01773680962e7b60c5
user-THINK\user <ddt.ootsuka@gmail.com>
2018-07-30 21:09:16 +0900

    再E1_1　２回目　コミットします。

--- kennsyuu1/src/kennsyuu1/E1_1_kouzou.java
+++ kennsyuu1/src/kennsyuu1/E1_1_kouzou.java
@@ -5,13 +5,17 @@ package kennsyuu1;
  */
 
 public class E1_1_kouzou {
-	//メインメソッドの宣言をします
+	// メインメソッドの宣言をします
 	public static void main(String[] args) {
 		//画面に"初めてのJavaプログラム。"と表示させる。
 		System.out.println("初めてのJavaプログラム。");
 		//画面に"画面に出力しています。"と表示させる。
-		System.out.println("画面に出力しています。");
-	}	
+		System.out.println("画面に出力しています。")
+	}
 }
 
-//結果は、セミコロンが欠如するとエラーとなる、でした。
+// 結果は、セミコロンが欠如すると、構文エラーとして下記文章が表示されました。
+「Exception in thread"main"java.lang.Error:
+Unresolved compilation problem:構文エラーがあります。";"
+を挿入して Statement　
+を完了してください at kennsyuu1.E1_1_kouzou.main(E1_1_kouzou.java:13)」
