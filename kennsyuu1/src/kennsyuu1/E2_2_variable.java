c90937c50506c0f59b9a0a1b5da369afdb61bd4f
user-THINK\user <ddt.ootsuka@gmail.com>
2018-08-20 21:29:43 +0900

    ーーーーーーーーーーーーー ［WIP］E2_2
    E2_1コメントの修正が完了、E2_2のプログラム作成に入っております＋55％（全体の内容）
    次回TODO　E2_2を完了とさせたいです。
    ーーーーーーーーーーーーー

---
+++ kennsyuu1/src/kennsyuu1/E2_2_variable.java
@@ -0,0 +1,32 @@
+package kennsyuu1;
+
+/*
+ * 三つのint型変数に値を代入し、合計と平均を求めるプログラムを作成せよ。
+ *目的：平均と合計を求める
+ *解き方：３つの値のそれぞれ合計と平均を求めていく
+ */
+
+public class E2_2_variable {
+	//メインメソッドを宣言します
+	public static void main(String[] args) {
+		
+		//int型の変数、variable1
+		int variable1 = 5
+		//int型の変数、variable2
+		int variable2 = 3
+		//int型の変数、variable3
+		int variable3 = 2
+		
+		//variable1に5を代入する
+		int variable1 = 5
+		//variable2に3を代入する
+		int variable2 = 3
+		//variable3に2を代入する
+		int variable3 = 2
+		
+		
+		
+		
+	}
+	
+}
