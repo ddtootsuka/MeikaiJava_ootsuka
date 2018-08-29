7ba1b9993fef81b4dac862371abb883087b29f5d
user-THINK\user <ddt.ootsuka@gmail.com>
2018-08-29 21:29:57 +0900

    ［WIP］E2_4
    E2_3のコメント（解き方）の修正とE2_4を進めた＋30％
    次回TODO　E2_4を完了させ、E2_5を進ませる。

---
+++ kennsyuu1/src/kennsyuu1/E2_4_variable.java
@@ -0,0 +1,17 @@
+package kennsyuu1;
+
+/*
+ * 右に示すように、キーボードから読み込んだ整数値に10を加えた値と10を減じた値を出力するプログラムを作成せよ
+ * 目的：整数値に10を加えた数、10を引いた数を表示させる
+ * 解き方：キーボードから読みこんだ数値に加算、減産行い、画面に表示させる
+ */
+
+public class E2_4_variable {
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+	}
+	
+	
+}
