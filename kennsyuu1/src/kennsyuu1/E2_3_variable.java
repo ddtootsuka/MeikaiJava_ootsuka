7b253815e595ce43cc3cf8e84f611bd6db004b00
user-THINK\user <ddt.ootsuka@gmail.com>
2018-08-29 20:29:37 +0900

    E2_3 1回目　コミットします。

--- kennsyuu1/src/kennsyuu1/E2_3_variable.java
+++ kennsyuu1/src/kennsyuu1/E2_3_variable.java
@@ -3,7 +3,7 @@ package kennsyuu1;
 /*
  * 右に示すように、キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成せよ。
  * 目的：示された数字をそのまま表示させる
- * 解き方：整数値を読み込んで、
+ * 解き方：キーボードから打った整数値を読み込んで、
  */
 
 	import java.util.Scanner;
@@ -14,10 +14,13 @@ public class E2_3_variable {
 		public static void main(String[] args) {
 			Scanner stdIn = new Scanner(System.in);
 			
+			//画面に"整数値："と表示させます
 			System.out.print("整数値：");
-
 			
+			//キーボードから読み込んだ数値をそのまま反復して表示させるために、integerに整数値を読み込む
+			int integer = stdIn.nextInt();
+			
+			//"○と入力しましたね"と画面に表示させる
+			System.out.println(integer + "と入力しましたね。");
 		}
-	
-
 }
