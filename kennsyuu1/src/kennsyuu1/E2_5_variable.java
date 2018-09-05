bb7046a36091e072eda683b8865ecc214f978263
user-THINK\user <ddt.ootsuka@gmail.com>
2018-09-05 21:29:23 +0900

    ［WIP］E2_6
    E2_4をプッシュまで行う、そしてE2_5の作成に入った＋100％（全体の内容）
    次回TODO（WIP)2_5を完成させる

---
+++ kennsyuu1/src/kennsyuu1/E2_5_variable.java
@@ -0,0 +1,13 @@
+package kennsyuu1;
+
+
+/*
+ * 二つの実数値を読み込み、その和と平均を求めて表示するプログラムを作成せよ
+ * 目的：二つの実数値の合計と平均を求める
+ * 解き方：実数値を読み込ませ、和と平均を画面に表示させる
+ */
+import java.util.Scanner;
+
+public class E2_5_variable {
+
+}
