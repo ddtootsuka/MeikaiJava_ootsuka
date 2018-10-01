a4d56af0bfb5ed3f3f15cbc66505214ac3654018
user-THINK\user <ddt.ootsuka@gmail.com>
2018-10-01 21:28:31 +0900

    ［WIP］E2_7
    E2_7を進めましたが、謎のエラーにより進めなくなってしまいました。70％（全体の内容）
    次回E2_7の問題を解決し終わらせること

--- kennsyuu1/src/kennsyuu1/E2_7_variable.java
+++ kennsyuu1/src/kennsyuu1/E2_7_variable.java
@@ -15,16 +15,25 @@ public class E2_7_variable {
 	
 //メインメソッドを宣言する
 	public static void main(String[] args) {
-		Random rand = new Random();
+		Random variable = new Random();
 		
 		//Randomnumber1に１以上９以下の値がランダムに入るようにする
-		int Randomnumber1 = rand.nextInt(9)+1;
-		//Randomnumber2に-９以上-１以下の値がランダムに入るようにする
-		int Randomnumber2 = rand.nextInt(-1)-9;		
+		int Randomnumber1 = variable.nextInt(9)+1;
 		
+		//Randomnumber2に-９以上-１以下の値がランダムに入るようにする
+		int Randomnumber2 = variable.nextInt(-1)-9;
+
+		//Randomnumber3に１０以上９９以下の値がランダムに入るようにする
+		int Randomnumber3 = variable.nextInt(99)+10;
+		
+		//画面に"一桁の正の整数値は"+ Randomnumber1 + "です。"を表示させる
 		System.out.println("一桁の正の整数値は"+ Randomnumber1 + "です。");
 		
+		//画面に"一桁の負の整数値は"+ Randomnumber2 + "です。"を表示させる
 		System.out.println("一桁の負の整数値は"+ Randomnumber2 + "です。");
+		
+		//画面に"二桁の正の整数値は"+ Randomnumber3 + "です。"を表示させる
+		System.out.println("二桁の正の整数値は"+ Randomnumber3 + "です。");
 	}
 }
 
