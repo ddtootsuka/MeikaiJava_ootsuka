d788f9a5ceea49c06dd4ae48c9f63140fc7d9ca3
user-THINK\user <ddt.ootsuka@gmail.com>
2018-10-10 21:29:15 +0900

    ［WIP］E2_8
    E2_7を完了とさせた＋100％
    次回TODO:E2_8を提出し、E2_9に入りたい（WIP)

--- kennsyuu1/src/kennsyuu1/E2_8_ransu.java
+++ kennsyuu1/src/kennsyuu1/E2_8_ransu.java
@@ -1,12 +1,28 @@
 package kennsyuu1;
 
-/*
- * キーボードから読み込んだ整数値プラスマイナス５の範囲の整数値をランダムに生成してひょうじすｓるプログラムを作成せよ。
- * 目的：
- * 解き方：
- */
+import java.util.Scanner;
 
+/*
+ * キーボードから読み込んだ整数値プラスマイナス５の範囲の整数値をランダムに生成して表示するプログラムを作成せよ。
+ * 目的：読み込んだ値のプラスマイナス５の範囲の整数値をランダムに生成し、表示させる
+ * 解き方：整数値プラスマイナス5の範囲を表す乱数をまず作成する
+ */
 
 public class E2_8_ransu {
 
+	//メインメソッドを宣言する
+		public static void main(String[] args) {
+			Scanner stdIn = new Scanner(System.in);
+			
+			//適当二世数値をうちこむ
+			System.out.print("整数値：");
+	
+			int randomnum = stdIn.nextInt(5)-5;
+	
+			//画面に"その値の"
+			System.out.println("その値の±５の乱数を生成しました。それは" + randomnum + "です");
+			
+			
+		}
+		
 }
