4e1276e42b5d6a35751dd5b6fd73ac673ce72a7b
user-THINK\user <ddt.ootsuka@gmail.com>
2018-10-22 21:28:04 +0900

    ［WIP］E2_9
    E2_9を提出し、E2_10に入った　＋100％
    次回TODO（WIP)E2_9の修正を終わらせる、そしてE2_10を提出させる

--- kennsyuu1/src/kennsyuu1/E2_9_randomnumber.java
+++ kennsyuu1/src/kennsyuu1/E2_9_randomnumber.java
@@ -8,7 +8,8 @@ import java.util.Random;
 /**
  * 以下に示すプログラムを作成せよ（実数値の乱数の生成にはnextDouble()を使うこと）
  * ・0.0以上1.0未満の実数値をランダムに生成して表示
- * ・1.0以上1.0未満の実数値をランダムに生成して表示 ・0.0以上10.0未満の実数値をランダムに生成して表示
+ * ・1.0以上1.0未満の実数値をランダムに生成して表示 
+ * ・0.0以上10.0未満の実数値をランダムに生成して表示
  * ・-1.0以上1.0未満の実数値をランダムに生成して表示 
  * 目的：各課題の指定の実数値の値をランダムに表示させる
  * 解き方：ランダムに生成する値の範囲に注意しながらランダム変数を使う
@@ -21,22 +22,22 @@ public class E2_9_randomnumber {
 		Random rand = new Random();
 
 		// 0.0以上1.0未満の乱数を生成する
-		double Randomnum1 = rand.nextDouble();
+		double RandomNum1 = rand.nextDouble();
 
-		// 画面に"0.0以上1.0未満の実数値は"+ Randomnumber1 + "です。"を表示させる
-		System.out.println("0.0以上1.0未満の実数値は" + Randomnum1 + "です。");
+		// 画面に"0.0以上1.0未満の実数値は"+ RandomNum1 + "です。"を表示させる
+		System.out.println("0.0以上1.0未満の実数値は" + RandomNum1 + "です。");
 
 		// 0.0以上10.0未満の乱数を生成する
-		double Randomnum2 = rand.nextDouble() + 8.9;
+		double RandomNum2 = rand.nextDouble() + 9.0;
 
-		// 画面に"0.0以上10.0未満の実数値は"+ Randomnumber1 + "です。"を表示させる
-		System.out.println("0.0以上10.0未満の実数値は" + Randomnum2 + "です。");
+		// 画面に"0.0以上10.0未満の実数値は"+ RandomNum2 + "です。"を表示させる
+		System.out.println("0.0以上10.0未満の実数値は" + RandomNum2 + "です。");
 
 		// -1.0以上1.0未満の乱数を生成する
-		double Randomnum3 = rand.nextDouble() - 1.0;
+		double RandomNum3 = rand.nextDouble() - 1.0;
 
-		// 画面に"-1.0以上1.0未満の実数値は"+ Randomnumber3 + "です。"を表示させる
-		System.out.println("-1.0以上1.0未満の実数値は" + Randomnum3 + "です。");
+		// 画面に"-1.0以上1.0未満の実数値は"+ RandomNum3 + "です。"を表示させる
+		System.out.println("-1.0以上1.0未満の実数値は" + RandomNum3 + "です。");
 
 	}
 }
