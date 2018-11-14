006e10e86899cd373ab75a38f04f69829e8fcf6a
user-THINK\user <ddt.ootsuka@gmail.com>
2018-11-14 21:15:51 +0900

    E3_2　1回目　コミットします。

--- kennsyuu1/src/kennsyuu1/E3_2_if2.java
+++ kennsyuu1/src/kennsyuu1/E3_2_if2.java
@@ -1,5 +1,36 @@
 package kennsyuu1;
 
+import java.util.Scanner;
+
+/*
+ *二つの正の整数値を読み込んで、後者が前者の約数であれば「BはAの約数です。」と表示し、
+ *そうでなければ「BはAの約数ではありません。」と表示するプログラムを作成せよ。
+ *目的：AとBを比較して約数であるか、そうでないかを判別させる
+ *解き方：AとBを比較し、約数であれば適する文言を表示させ、約数でなければそれに適する文言を表示させる。
+*/
+
 public class E3_2_if2 {
 
+	// メインメソッドを宣言する
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+
+		// 画面に"整数A：" と表示させる
+		System.out.println("整数A：");
+		// キーボードから読み込ませた値をvariableaに宣言する
+		int variablea = stdIn.nextInt();
+
+		// 画面に"整数B：" と表示させる
+		System.out.println("整数B：");
+		// キーボードから読み込ませた値をvariablebに宣言する
+		int variableb = stdIn.nextInt();
+
+		// variableaとvariablebを比較させ、variableaがvariablebで割り切れれば"BはAの約数です"と画面に表示させ、割り切れなければ"BはAの約数ではありません"と画面に表示させる
+		if (variablea > variableb)
+			if (variablea % variableb == 0) {
+				System.out.println("BはAの約数です");
+			} else {
+				System.out.println("BはAの約数ではありません");
+			}
+	}
 }
