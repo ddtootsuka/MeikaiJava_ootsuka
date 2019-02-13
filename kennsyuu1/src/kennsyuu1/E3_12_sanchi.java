28893b2b16f64df249d264ff067050f5fac851be
user-THINK\user <ddt.ootsuka@gmail.com>
2019-02-13 21:09:38 +0900

    E_3_12　２回目　修正しました。

--- kennsyuu1/src/kennsyuu1/E3_12_sanchi.java
+++ kennsyuu1/src/kennsyuu1/E3_12_sanchi.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 /*
  * キーボードから読み込んだ三つに整数値の最小値を求めて表示するプログラムを作成せよ
- * 目的：３つの整数値を比較して一番小さい値を求める
- * 解き方：Variable1から、2と3を順に比較していき最小値を求める
+ * 目的：3つの整数値を比較して一番小さい値を求める
+ * 解き方：整数1から、2と3を順に比較していき最小値を求める
  */
 
 public class E3_12_sanchi {
@@ -14,25 +14,25 @@ public class E3_12_sanchi {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 
-		// 画面に"Variale1:"と表示させる。Variable1にキーボードから読み込んだ値を代入する。
-		System.out.print("Variable1:");
-		int Variable1 = stdIn.nextInt();
+		// 画面に"Integer1:"と表示させる。Integer1にキーボードから読み込んだ値を代入する。
+		System.out.print("Integer1:");
+		int Integer1 = stdIn.nextInt();
 
-		// 画面に"Variable2:"と表示させる。Variable2にキーボードから読み込んだ値を代入する。
-		System.out.print("Variable2:");
-		int Variable2 = stdIn.nextInt();
+		// 画面に"Integer2:"と表示させる。Integer2にキーボードから読み込んだ値を代入する。
+		System.out.print("Integer2:");
+		int Integer2 = stdIn.nextInt();
 
-		// 画面に"Variable3:"と表示させる。Variable3にキーボードから読み込んだ値を代入する。
-		System.out.print("Variable3:");
-		int Variable3 = stdIn.nextInt();
+		// 画面に"Integer3:"と表示させる。Integer3にキーボードから読み込んだ値を代入する。
+		System.out.print("Integer3:");
+		int Integer3 = stdIn.nextInt();
 
-		// minimumにまずVariable1を代入し、Variable2と比較させVariable2の方が小さければminimumにVariable2の値を代入する。そうでなければ、Variable3とminimumを比較しVariable3の方が小さければVariable3の値を代入する。
-		int minimum = Variable1;
-		if (Variable2 < minimum) {
-			minimum = Variable2;
+		// minimumにまずInteger1を代入し、Integer2と比較させInteger2の方が小さければminimumにInteger2の値を代入する。そうでなければ、Integer3とminimumを比較しInteger3の方が小さければInteger3の値を代入する。
+		int minimum = Integer1;
+		if (Integer2 < minimum) {
+			minimum = Integer2;
 		}
-		if (Variable3 < minimum) {
-			minimum = Variable3;
+		if (Integer3 < minimum) {
+			minimum = Integer3;
 		}
 
 		// 画面に"最小値は(minimumに入る値)です"と表示させる。
