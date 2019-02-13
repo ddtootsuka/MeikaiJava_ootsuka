67eda1eea0961d7e10a710de0723b13a78e2567d
user-THINK\user <ddt.ootsuka@gmail.com>
2019-02-13 21:30:37 +0900

    ［WIP］E3_13
    E3_11,E3_12の修正、E3_13の完成（未提出）+５０％
    次回TODO　E3_14にはいる（WIP)

--- kennsyuu1/src/kennsyuu1/E_3_13_sanntinosaidaiti.java
+++ kennsyuu1/src/kennsyuu1/E_3_13_sanntinosaidaiti.java
@@ -30,18 +30,19 @@ public class E_3_13_sanntinosaidaiti {
 
 		// 中央値には整数値Aを入れます
 		int median = IntegerA;
-
-		// 整数値Bと中央値を比較し、整数値Bの方が大きければそのまま値を代入する
-		if (IntegerB > median)
-			median = IntegerB;
-		{
-
-			// 整数値Cと中央値を比較し、整数値Cの方が大きければそのまま値を代入する
-		}
-		if (IntegerC < median) {
-			median = IntegerC;
-		}
+		
+		// 整数値Bと中央値を比較し、整数値Bの方が小さければそのまま値を代入する		
+		if(IntegerB > median) { median = IntegerB;
+		// 整数値Cと中央値を比較し、整数値Cの方が小さければそのまま値を代入する	
+		if(IntegerC > median) median = IntegerC;}
+		
+		if(IntegerB < median) {median = IntegerB;
+		if(IntegerC > median) median = IntegerC;}
+			
 		// 画面に"中央値は〇です。"と表示させる
 		System.out.println("中央値は" + median + "です。");
 	}
 }
+
+
+	
